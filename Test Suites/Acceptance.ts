c560e684f173136688d01f1b557cf294a2d97152
akhilesh <akhilesh@172.16.16.173>
2019-12-03 18:06:02 +0530

    Adding Acceptance Suite

---
+++ Test Suites/Acceptance.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Acceptance</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>db6d4920-abeb-4cd1-ad3c-b58501d77695</testSuiteGuid>
+</TestSuiteEntity>
